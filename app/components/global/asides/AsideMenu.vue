21fec86bd9f1e82c39589fd5e4f5107d847c80c4
Esteban Ramos Fernandez <esteban.ramos.f@gmail.com>
2022-11-15 14:05:15 -0300

    Update, fix deps

---
+++ app/components/global/asides/AsideMenu.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  aside.sticky-aside
+    section.m-1
+      header
+        //- h1.py-3 Adicionales
+</template>
+<script>
+
+export default {
+  components: {
+
+  },
+  'aside-menu': {
+    props: [''],
+    'name': 'aside-menu',
+    data () {
+      return {}
+    },
+    ready () {},
+    created () {},
+    filters: {},
+    methods: {}
+  }
+}
+</script>
+<style>
+</style>
